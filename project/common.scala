b5aabb4e109282a00a196973899ae27139b49eca
Viktor Taranenko <viktortnk@gmail.com>
2013-11-19 13:58:13 +0000

    publish configuration

--- project/common.scala
+++ project/common.scala
@@ -4,15 +4,24 @@ import com.typesafe.sbt.SbtScalariform.scalariformSettings
 
 object common {
 
+  val gitHeadCommitSha = settingKey[String]("current git commit SHA")
+
   val commonSettings = Seq(
-    organization := "whisk",
-    scalaVersion := "2.10.2")
+    organization := "com.whisk",
+    scalaVersion := "2.10.3",
+    gitHeadCommitSha := Process("git rev-parse --short HEAD").lines.head)
     
   def module(name: String) =
     Project(name, file(name))
     .settings(commonSettings:_*)
     .settings(scalariformSettings:_*)
     .settings(
-      resolvers += "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+      version := "0.1.0-" + gitHeadCommitSha.value,
+      resolvers += "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      publishTo := {
+        val dir = if (version.value.trim.endsWith(gitHeadCommitSha.value)) "snapshots" else "releases"
+        val repo = Path.userHome / "mvn-repo" / dir
+        Some(Resolver.file("file", repo) )
+      }
     )
 }
