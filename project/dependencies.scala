494a015031e95cdf3201d38bcaa4c3bb705c5cc7
Viktor Taranenko <viktortnk@gmail.com>
2015-09-07 17:24:24 +0100

    reactivemongo 0.11.7

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object dependencies {
 
   object V {
-    val Reactivemongo = "0.11.6"
+    val Reactivemongo = "0.11.7"
   }
 
-  val playJson = "com.typesafe.play" %% "play-json" % "2.4.2"
+  val playJson = "com.typesafe.play" %% "play-json" % "2.4.3"
   val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % V.Reactivemongo
-  val playReactivemongo = "org.reactivemongo" %% "play2-reactivemongo" % "0.11.6.play24"
+  val playReactivemongo = "org.reactivemongo" %% "play2-reactivemongo" % "0.11.7.play24"
 
   val jodaTime = "joda-time" % "joda-time" % "2.8.1"
   val jodaConvert = "org.joda" % "joda-convert" % "1.7"
