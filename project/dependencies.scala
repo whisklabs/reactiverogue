e1ec42648fb848c7b334cf1bb24ce2662f1abd87
Viktor Taranenko <viktortnk@gmail.com>
2016-07-08 14:26:12 +0100

    update dependencies

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 object dependencies {
 
   object V {
-    val Reactivemongo = "0.11.11"
+    val Reactivemongo = "0.11.14"
   }
 
-  val playJson = "com.typesafe.play" %% "play-json" % "2.5.2"
+  val playJson = "com.typesafe.play" %% "play-json" % "2.5.4"
   val reactivemongoBson = "org.reactivemongo" %% "reactivemongo-bson" % V.Reactivemongo
   val reactivemongoIteratees = "org.reactivemongo" %% "reactivemongo-iteratees" % V.Reactivemongo
   val reactivemongoJson = "org.reactivemongo" %% "reactivemongo-play-json" % (V.Reactivemongo )
@@ -14,7 +14,7 @@ object dependencies {
 
   val junit = "junit" % "junit" % "4.12"
   val scalatest = "org.scalatest" %% "scalatest" % "2.2.6"
-  val dockerIt = "com.whisk" %% "docker-testkit-scalatest" % "0.7.0-RC3"
+  val dockerIt = "com.whisk" %% "docker-testkit-scalatest" % "0.8.2"
 
   val bsonDependencies =
     Seq(reactivemongoBson)
